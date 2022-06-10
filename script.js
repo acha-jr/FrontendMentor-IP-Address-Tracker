81a2cfbf631223233878afd4c61467d0cf547469
Obinna Achazie <105124616+acha-jr@users.noreply.github.com>
2022-06-10 12:06:15 +0100

    update script.js

--- script.js
+++ script.js
@@ -50,6 +50,12 @@ button.addEventListener("click", () => {
   let url = `https://geo.ipify.org/api/v2/country,city?apiKey=at_2WQbTZ9jnv0G1oPRzvzN8qEE5it0e&ipAddress=${input.value}`;
   fetchData(url);
 });
+input.addEventListener("keypress", (e) => {
+  if (e.key === "Enter") {
+    let url = `https://geo.ipify.org/api/v2/country,city?apiKey=at_2WQbTZ9jnv0G1oPRzvzN8qEE5it0e&ipAddress=${input.value}`;
+    fetchData(url);
+  }
+});
 
 window.addEventListener("load", () => {
   let url = `https://geo.ipify.org/api/v2/country,city?apiKey=at_2WQbTZ9jnv0G1oPRzvzN8qEE5it0e&ipAddress=`;

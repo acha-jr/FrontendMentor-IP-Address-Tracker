3b5a6ba653ac8c49962da84575efb16772e41e03
Obinna Achazie <105124616+acha-jr@users.noreply.github.com>
2022-06-10 11:54:48 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,57 @@
+const ipAddress = document.querySelector(".IP");
+const ipLocation = document.querySelector(".location");
+const city = document.querySelector(".city");
+const region = document.querySelector(".region");
+const postcode = document.querySelector(".postcode");
+const timezone = document.querySelector(".timezone");
+const isp = document.querySelector(".ISP");
+
+const input = document.querySelector("input");
+const button = document.querySelector("button");
+
+const fetchData = (apiurl) => {
+  fetch(apiurl)
+    .then((resp) => {
+      return resp.json();
+    })
+    .then((data) => {
+      ipAddress.textContent = data.ip;
+      city.textContent = data.location.city;
+      region.textContent = data.location.region;
+      postcode.textContent = data.location.postalCode;
+      timezone.textContent = data.location.timezone;
+      isp.textContent = data.isp;
+
+      let lat = data.location.lat;
+      let long = data.location.lng;
+
+      setLocation(lat, long);
+    });
+};
+
+var map = L.map("map");
+L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+  attribution:
+    '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors </br> Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a> Coded by <a href="https://github.com/acha-jr" target="_blank">acha-jr</a>.',
+}).addTo(map);
+
+const setLocation = (lat, long) => {
+  map.setView([lat, long], 13);
+
+  var myIcon = L.icon({
+    iconUrl: "./images/icon-location.svg",
+    iconSize: [46, 56],
+    iconAnchor: [22, 94],
+  });
+  L.marker([lat, long], { icon: myIcon }).addTo(map);
+};
+
+button.addEventListener("click", () => {
+  let url = `https://geo.ipify.org/api/v2/country,city?apiKey=at_2WQbTZ9jnv0G1oPRzvzN8qEE5it0e&ipAddress=${input.value}`;
+  fetchData(url);
+});
+
+window.addEventListener("load", () => {
+  let url = `https://geo.ipify.org/api/v2/country,city?apiKey=at_2WQbTZ9jnv0G1oPRzvzN8qEE5it0e&ipAddress=`;
+  fetchData(url);
+});
